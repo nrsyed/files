dfc6a46e85595c78552ad8a2e5c6bd589e77992c
Najam Syed <najam.r.syed@gmail.com>
2019-04-10 07:08:33 -0400

    Add install_opencv.sh

--- bash_scripts/install_opencv.sh
+++ bash_scripts/install_opencv.sh
@@ -9,22 +9,25 @@ sudo apt install -y libgtk2.0-dev libgtk-3-dev
 sudo apt install -y libatlas-base-dev gfortran
 sudo apt install -y python2.7-dev python3-dev
 
-cd ~
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
+mkdir ~/envs
+cd ~/envs
 virtualenv -p python3 cv_env
 source cv_env/bin/activate
 pip install numpy
 
-source cv_env/bin/activate
-cd opencv
+cd ~
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+cd ~/opencv
 mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
   -D CMAKE_INSTALL_PREFIX=/usr/local \
   -D INSTALL_PYTHON_EXAMPLES=ON \
   -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-  -D BUILD_EXAMPLES=ON ..
+  -D PYTHON_EXECUTABLE=~/envs/cv_env/bin/python \
+  -D BUILD_EXAMPLES=ON \
+  -D BUILD_OPENCV_PYTHON3=YES ..
 
 make -j4
 sudo make install
