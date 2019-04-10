891a93780c42585f2c36fafdae9043da4be81280
Najam Syed <najam.r.syed@gmail.com>
2019-04-10 07:08:20 -0400

    Update ubuntu_setup.sh

---
+++ bash_scripts/ubuntu_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASICS="arp-scan bzip2 chromium-browser dropbox evince firefox gzip hplip hplip-gui redshift transmission"
+MEDIA="audacity brasero browser-plugin-vlc gimp lame openshot texstudio vlc vokoscreen"
+DEV="arduino build-essential chromium-chromedriver git octave openssh-server python-pip python3-pip virtualbox valgrind tesseract-ocr testdisk vim"
+EXTRA="lm-sensors clamav clamtk flashplugin-installer gnome-tweak-tool pdfshuffler p7zip p7zip-full"
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y $BASICS
+sudo apt install -y $DEV
+sudo apt install -y $MEDIA
+sudo apt install -y $EXTRA
+
+pip install virtualenv virtualenvwrapper
+sudo ln -s ~/.local/bin/virtualenv /usr/local/bin/virtualenv
+
+# TODO: 18.04 specific stuff: tweak, config and .*rc files, 
