3633651a646adea04abb17333ba8d478586809ef
Najam Syed <najam.r.syed@gmail.com>
2020-06-02 23:59:53 -0400

    Add system monitoring tools tmux script

---
+++ scripts/tmux_monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+session=monitor
+
+tmux new-session -d -s $session
+tmux send-keys -t 0 'htop' C-m
+
+tmux split-window -v -p 20 "watch 'sensors | egrep \"Tdie|Tctl|fan\"'"
+tmux split-pane -t 0 -h 'nvtop'
+tmux split-pane -t 2 -h
+tmux send-keys -t 3 "workon aio" C-m
+tmux send-keys -t 3 "watch 'sudo \$(which liquidctl) status'" C-m
+
+tmux attach-session -t $session
