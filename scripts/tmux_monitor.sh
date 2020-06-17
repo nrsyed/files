c471888efa6a48332bdd28d0737c6abe7a126157
Najam Syed <najam.r.syed@gmail.com>
2020-06-17 00:29:44 -0400

    Add tmux_monitor.sh

--- scripts/tmux_monitor.sh
+++ scripts/tmux_monitor.sh
@@ -5,10 +5,10 @@ session=monitor
 tmux new-session -d -s $session
 tmux send-keys -t 0 'htop' C-m
 
-tmux split-window -v -p 20 "watch 'sensors | egrep \"Tdie|Tctl|fan\"'"
+tmux split-window -v -p 20 "watch -n 2 'sensors | egrep \"Tdie|Tctl|fan\"'"
 tmux split-pane -t 0 -h 'nvtop'
 tmux split-pane -t 2 -h
 tmux send-keys -t 3 "workon aio" C-m
-tmux send-keys -t 3 "watch 'sudo \$(which liquidctl) status'" C-m
+tmux send-keys -t 3 "watch -n 2 'sudo \$(which liquidctl) status'" C-m
 
 tmux attach-session -t $session
